31ead905bf863ca9716a637a55cfd748d27b4fcf
Josh Steinberg <jpsteinberg@gmail.com>
2021-04-28 17:09:24 -0400

    working on image frontend route, finished backend

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -2,11 +2,11 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { check, validationResult } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
-const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { Image } = require("../../db/models");
 
 const singlePublicFileUpload = require("../../awsS3");
 const singleMulterUpload = require("../../awsS3");
+const db = require("../../db/models");
 
 const router = express.Router();
 
@@ -14,10 +14,29 @@ const imageValidators = [
   check("imageUrl")
     .exists({ checkFalsy: true })
     .withMessage("Please provide an image url"),
+  handleValidationErrors,
 ];
 
+router.get(
+  "/user/:userId",
+  asyncHandler(async (req, res) => {
+    const userId = req.body.userId;
+    const profileImage = await db.Image.findByPk(userId);
+    res.json({ profileImage });
+  })
+);
+
+router.get(
+  "/beer/:beerId",
+  asyncHandler(async (req, res) => {
+    const beerId = req.body.beerId;
+    const beerImage = await db.Image.findByPk(beerId);
+    res.json({ beerImage });
+  })
+);
+
 router.post(
-  "/images",
+  "/",
   imageValidators,
   singleMulterUpload("image"),
   asyncHandler(async (req, res) => {
@@ -40,3 +59,29 @@ router.post(
     }
   })
 );
+
+const remove = async (id) => {
+  const image = await db.Review.findByPk(id);
+  if (!image) throw new Error("Can't find that review");
+
+  await db.Image.destroy({ where: { id: image.id } });
+  return image.id;
+};
+
+router.delete(
+  "/user/:userId",
+  asyncHandler(async (req, res) => {
+    const userId = await remove(req.params.id);
+    return res.json({ userId });
+  })
+);
+
+router.delete(
+  "/beer/:beerId",
+  asyncHandler(async (req, res) => {
+    const beerId = await remove(req.params.id);
+    return res.json({ beerId });
+  })
+);
+
+module.exports = router;
